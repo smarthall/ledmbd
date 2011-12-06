5d1ceb3ffe29a796a00453ba9e30b59a02a139c6
Daniel Hall <daniel.hall@rea-group.com>
2011-12-06 22:33:06 +1100

    Adding structs to store current image on display

--- displaythread.h
+++ displaythread.h
@@ -1,6 +1,17 @@
-
 #ifndef _DISPLAYTHREAD_H
 #define _DISPLAYTHREAD_H
+
+#include <stdint.h>
+
+#define X_SIZE 21
+#define Y_SIZE 7
+
+#define GETLED(x,y) (x)
+
+typedef struct struct_led_display {
+	uint8_t data[21];
+} led_display;
+
 
 #endif
 
